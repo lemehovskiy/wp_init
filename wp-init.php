7202c6a0058f143cc774627d3a19d37f07724a97
lemehovskiy <lemehovskiy@gmail.com>
2017-10-24 21:22:29 +0300

    remove base plugins, themes, download starter theme

--- wp-init.php
+++ wp-init.php
@@ -1,42 +1,80 @@
 <?php
 
 
+$init_settings = array(
+    'project_name' => 'test_theme'
+);
+
+
+$base_themes = array(
+    'wp-content/themes/twentyfifteen',
+    'wp-content/themes/twentyseventeen',
+    'wp-content/themes/twentysixteen'
+);
+
+$base_plugins = array(
+    'wp-content/plugins/akismet',
+    'wp-content/plugins/hello.php'
+);
+
+
 function dowload_wp()
 {
-    $url = "https://wordpress.org/latest.zip";
+    system('curl -L -o latest.zip https://wordpress.org/latest.zip');
 
-    $path = 'filename.zip'; //address of local file
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $con = curl_exec($ch);
-    curl_close($ch);
-    file_put_contents($path, $con);
 }
 
 
 function extract_wp()
 {
-    system('tar -xvf filename.zip --strip 1');
+    system('tar -xvf latest.zip --strip 1');
 }
 
-
-function delete_base_themes()
+function delete_wp_archive()
 {
-    system('export GLOBIGNORE=wp-content/themes/index.php');
-    system('rm -rf wp-content/themes/*');
-    system('export GLOBIGNORE=');
+    system('rm latest.zip');
 }
 
 
-function delete_base_plugins()
+function delete_base_themes($base_themes)
 {
-    system('GLOBIGNORE=*/index.php');
-    system('rm -rf wp-content/plugins/*');
-    system('unset GLOBIGNORE');
+    foreach ($base_themes as $theme) {
+        system('rm -rf ' . $theme);
+    }
 }
 
 
-//dowload_wp();
+function delete_base_plugins($base_plugins)
+{
+    foreach ($base_plugins as $plugin) {
+        system('rm -rf ' . $plugin);
+    }
+}
+
+function download_starter_theme()
+{
+    system('curl -L -o wp-starter-theme.zip https://github.com/lemehovskiy/wp-starter-theme/archive/master.zip');
+}
+
+function extract_starter_theme($init_settings)
+{
+    system('mkdir -p wp-content/themes/' . $init_settings['project_name']);
+    system('tar -xvf wp-starter-theme.zip --strip 1 --directory wp-content/themes/' . $init_settings['project_name']);
+}
+
+function delete_starter_theme_archive()
+{
+    system('rm wp-starter-theme.zip');
+}
+
+
+dowload_wp();
 extract_wp();
-//delete_base_themes();
-//delete_base_plugins();
+delete_wp_archive();
+delete_base_themes($base_themes);
+delete_base_plugins($base_plugins);
+
+
+download_starter_theme();
+extract_starter_theme($init_settings);
+delete_starter_theme_archive();
